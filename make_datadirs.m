fd6c7cf8653e57761f23f066815b7afe0e3f8b7e
Lucas Jeay-Bizot <jeaybizot.lucas@gmail.com>
2025-06-07 15:17:54 -0700

    Create make_datadirs.m
    add script to populate the necessary subfolders in data\

---
+++ make_datadirs.m
@@ -0,0 +1,14 @@
+if ~isdir('data')
+    mkdir data
+    
+    mkdir data\new
+    mkdir data\original
+    mkdir data\simulated
+    
+    mkdir data\new\raw
+    mkdir data\new\processed
+    
+    mkdir data\original\processed
+    
+    mkdir data\simulated\processed
+end
